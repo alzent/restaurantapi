15371e2249a71091f20fac7174a3a2c5c94ecc29
alzent <azent@gmx.de>
2023-02-19 16:58:44 +0100

    modified

--- src/main/java/de/zent/restaurantapi/RestaurantapiApplication.java
+++ src/main/java/de/zent/restaurantapi/RestaurantapiApplication.java
@@ -2,12 +2,19 @@ package de.zent.restaurantapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestaurantapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantapiApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	String sayHello() {
+		return "Hello Azure 25!";
+	}
 }
